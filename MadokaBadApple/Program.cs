a2c112050bc0cc2890d6ad1fe1f4a2bb017ef4b3
Jesus Alvarez Sombrerero <93137954+dontloseyourheadsu@users.noreply.github.com>
2024-03-31 12:24:09 -0600

    Madoka Magica badapple terminal

---
+++ MadokaBadApple/Program.cs
@@ -0,0 +1,61 @@
+﻿using NAudio.Wave;
+using OpenCvSharp;
+
+const string grayScale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+const string videoPath = @"madoka-magica-bad-apple.mp4";
+const string audioPath = @"bad-apple.mp3";
+
+const int outputWidth = 50;
+
+using var capture = new VideoCapture(videoPath);
+if (!capture.IsOpened())
+{
+    Console.WriteLine("Error: Video file could not be opened.");
+    return;
+}
+
+using (var audioOutput = new WaveOutEvent())
+using (var audioFile = new AudioFileReader(audioPath))
+{
+    audioOutput.Init(audioFile);
+    audioOutput.Play();
+
+    while (true)
+    {
+        using var frameMat = new Mat();
+        if (!capture.Read(frameMat) || frameMat.Empty())
+        {
+            capture.Set(VideoCaptureProperties.PosFrames, 0);
+            continue;
+        }
+
+        var asciiArt = ConvertToAscii(frameMat, outputWidth);
+        Console.Clear();
+        Console.WriteLine(asciiArt);
+        Thread.Sleep(26);
+    }
+}
+
+string ConvertToAscii(Mat frame, int outputWidth)
+{
+    var scaleFactor = outputWidth / (double)frame.Width;
+    var outputHeight = (int)(frame.Height * scaleFactor);
+
+    Cv2.Resize(frame, frame, new Size(outputWidth, outputHeight));
+
+    Cv2.CvtColor(frame, frame, ColorConversionCodes.BGR2GRAY);
+
+    var asciiArt = "";
+    for (int y = 0; y < frame.Rows; y++)
+    {
+        for (int x = 0; x < frame.Cols; x++)
+        {
+            byte color = frame.At<byte>(y, x);
+            int index = color * (grayScale.Length - 1) / 255;
+            asciiArt += grayScale[index];
+        }
+        asciiArt += "\n";
+    }
+
+    return asciiArt;
+}
